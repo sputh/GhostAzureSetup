1bc719c2de6db882955b1a9615e5369374270c57
sputh <samantha.puth@gmail.com>
2015-01-19 13:00:24 -0800

    Configure commit

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://yourawesomewebsite.azurewebsites.net/',
+        url: 'http://coderskye.azurewebsites.net/',
         mail: {},
         database: {
             client: 'sqlite3',
